35ca6a23ec90810757d5e1a65ba7605c1dbc860f
unknown <fernandopascott@gmail.com>
2026-03-13 13:37:12 -0300

    Criação de POC para intregração de app PGE com IA Copilot e Open AI - Inicial

--- src/main/java/br/com/poc/pge/ia/IaGrauRecuperabilidadeApplication.java
+++ src/main/java/br/com/poc/pge/ia/IaGrauRecuperabilidadeApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OpenAiDemoApplication {
+public class IaGrauRecuperabilidadeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OpenAiDemoApplication.class, args);
+        SpringApplication.run(IaGrauRecuperabilidadeApplication.class, args);
     }
 }
